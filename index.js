559406ad0ef1c41cf628b049f07b4681c6a25113
sanoop <sanoopsam@icloud.com>
2023-03-16 20:08:51 +0530

    husky test commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const name = "sanoop"
+const express = require('express');
+const app = express();
+
+const PORT = 4001;
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port: ${PORT}`)
+})
